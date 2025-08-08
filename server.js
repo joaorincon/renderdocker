446782a6a3c9e476cabeab1e44ff5efad87d22a3
joaorincon <50960601+joaorincon@users.noreply.github.com>
2025-08-08 10:47:30 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -50,6 +50,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT, 10),
+  ssl: {
+    rejectUnauthorized: false
+  },
 });
 
 // Test DB connection
